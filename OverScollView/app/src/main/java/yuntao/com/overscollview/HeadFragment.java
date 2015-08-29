cdd240e0443c382c2d1a97f754db62a8c01a4bf2
Jomes <lu8876697@163.com>
2015-08-29 12:57:06 +0800

    headFragment 添加

--- OverScollView/app/src/main/java/yuntao/com/overscollview/HeadFragment.java
+++ OverScollView/app/src/main/java/yuntao/com/overscollview/HeadFragment.java
@@ -6,17 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 
 /**
  * Created by jomeslu on 15-8-28.
  */
 public class HeadFragment extends Fragment {
 
+    private ViewParent mViewParent;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = View.inflate(getActivity(), R.layout.head, null);
+        initView(view);
+        return view;
 
+    }
+
+    private void initView(View view) {
+        mViewParent= (ViewParent) view.findViewById(R.id.vPager);
     }
 }
